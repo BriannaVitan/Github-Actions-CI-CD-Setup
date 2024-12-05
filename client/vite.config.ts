954b5e97a60a9449d97fbe1c84ed1b5dff08ebed
BriannaVitan <briannavitan@gmail.com>
2024-12-05 09:19:57 -0600

    added changes to vite's

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     open: true,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3001',
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    host: '0.0.0.0', // Use 0.0.0.0 to listen on all IPs in production
+    port: 3001,
+    https: false, // Ensure it's set to false unless explicitly required
+  },
+  build: {
+    outDir: 'dist',
   },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,38 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vitest/config'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   test: {
+//     globals: true,
+//     environment: 'jsdom',
+//     setupFiles: './src/_tests_/setup.ts'
+//   },
+//   server: {
+//     port: 3000,
+//     open: true,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3001',
+//         secure: false,
+//         changeOrigin: true
+//       }
+//     }
+//   }
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/_tests_/setup.ts'
-  },
   server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        secure: false,
-        changeOrigin: true
-      }
-    }
-  }
-})
+    host: '0.0.0.0', // Use 0.0.0.0 to listen on all IPs in production
+    port: 3001,
+    https: false, // Ensure it's set to false unless explicitly required
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
