ff1b4a9663d42b0dea969b6c6da064da7d38432c
BriannaVitan <briannavitan@gmail.com>
2024-12-05 15:20:27 -0600

    added dirname to server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,18 @@ import express from 'express';
 // import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
+import path from 'path'
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const buildPath = path.join(__dirname, "../../client/dist")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serves static files in the entire client's dist folder
-app.use(express.static('../../../client/dist'));
+// app.use(express.static('../../../client/dist'));
+app.use(express.static(buildPath));
 
 app.use(routes);
 
