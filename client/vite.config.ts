4060d2d3c611454d030d9d1e68b7d44f89ffc205
BriannaVitan <briannavitan@gmail.com>
2024-12-10 13:11:48 -0600

    changes to vite.config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,27 +1,21 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/_tests_/setup.ts'
-  },
   server: {
     port: 3000,
     open: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
+        changeOrigin: true,
         secure: false,
-        changeOrigin: true
-      }
-    }
-  }
-})
-
+      },
+    },
+  },
+});
 
 // import { defineConfig } from 'vite';
 // import react from '@vitejs/plugin-react';
@@ -33,13 +27,13 @@ export default defineConfig({
 //     port: 3001,
 //     https: true, // Ensure it's set to false unless explicitly required
 //     proxy: {
-//             '/api': {
-//               target: 'https://github-actions-ci-cd-setup-lzts.onrender.com',
-//               secure: false,
-//               changeOrigin: true }
+//       '/api': {
+//         target: 'https://github-actions-ci-cd-setup-lzts.onrender.com',
+//         secure: false,
+//         changeOrigin: true }
+// },
 //   },
 //   build: {
 //     outDir: 'dist',
-    
 //   },
-// }});
+// });
